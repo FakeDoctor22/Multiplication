d8817132b5c5c243c930a70df0035143d0ef7a9f
FakeDoctor22 <giljoelvillarbaatwork@gmail.com>
2023-05-06 10:49:59 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Write a function called multiply that takes 2 numbers and returns their product.
+
+const multiply = (a, b) => {
+    return a * b;
+}
+
+console.log(multiply(3, 3));
+console.log(multiply(3, 7));
